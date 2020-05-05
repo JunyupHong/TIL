f5e089cb26ff20431b16ed1e83c99edeee74a933
JunyupHong <junyup.hong@gmail.com>
2020-05-05 18:32:48 +0900

    stack/queue 1 update

--- algorithm/programmers_stack:queue1.js
+++ algorithm/programmers_stack:queue1.js
@@ -1,34 +1,27 @@
-// function solution(heights) {
-//     const answer = [];
-// 	const stack = heights.slice(); // 깊은 복사
-// 	let right;
-// 	while (right = stack.pop()) {
-// 		if(stack.length === 0) answer.push(0); // 1번 인덱스 신호는 아무도 받을 수 없음
-// 		for(let i = stack.length - 1; i >= 0; i--) {
-// 			if(stack[i] > right) {
-// 				answer.push(i + 1); // index가 아닌 n번째이므로 +1 해줌
-// 				break;
-// 			} else if(i === 0) answer.push(0); // 아무도 받지않음
-// 		}
-// 	}
-//     answer.reverse();
-//     return answer;
-// }
-
 function solution(heights) {
-    const answer = [];
-    for(let i = 0; i < heights.length; i++) {
-    	const right = heights[i];
-    	if (i === 0) {
-    		answer.push(0); // 0번 인덱스 신호는 받을 수 없음
-    		continue;
-    	}
-    	for(let j = i - 1; j >= 0; j--) {
-    		if(heights[j] > right) {
-    			answer.push(j + 1);
-    			break;
-    		} else if(j === 0) answer.push(0);
-    	}
+    const answer = [0];
+    for (let i = 0; i < heights.length; i++) {
+        const stack = heights.slice(0, i + 1);
+        const signal = stack.pop();
+        while (stack.length) {
+            const h = stack.pop();
+            if (h > signal) {
+                answer.push(stack.length + 1);
+                break;
+            }
+            if(stack.length === 0) answer.push(0);
+        }
+    	// const signal = heights[i];
+    	// if (i === 0) {
+    	// 	answer.push(0); // 0번 인덱스 신호는 받을 수 없음
+    	// 	continue;
+    	// }
+    	// for (let j = i - 1; j >= 0; j--) {
+    	// 	if (heights[j] > signal) {
+    	// 		answer.push(j + 1);
+    	// 		break;
+    	// 	} else if (j === 0) answer.push(0);
+    	// }
     }
 
     return answer;
