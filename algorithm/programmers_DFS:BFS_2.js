ff7d8e831243a72c635998c28a7f170332878e8d
JunyupHong <junyup.hong@gmail.com>
2020-05-13 13:39:18 +0900

    programmers DFS/BFS 네트워크

--- algorithm/programmers_DFS:BFS_2.js
+++ algorithm/programmers_DFS:BFS_2.js
@@ -1,20 +1,74 @@
 function solution(n, computers) {
+    // dfs - stack
+//     let answer = 0;
+//     const visited = [];
+//     for (let i = 0; i < n; i++) {
+//         visited.push(false);
+//     }
+    
+//     for (let i = 0; i < n; i++) {
+//         if (visited[i]) continue;
+//         visited[i] = true;
+//         answer++;
+//         const stack = [i];
+//         while (stack.length) {
+//             const connection = computers[stack.pop()];
+//             connection.forEach((c, idx) => {
+//                 if(c && !visited[idx]) {
+//                     stack.push(idx);
+//                     visited[idx] = true;
+//                 }
+//             });
+//         }
+//     }
+//     return answer;
+    
+    
+    
+    
+    // dfs - recursion
     let answer = 0;
-    const check = computers.map(() => 0);
+    const visited = [];
     for (let i = 0; i < n; i++) {
-        if(!check[i]) {
-            answer++;
-            // bfs
-            const queue = [i];
-            while (queue.length > 0) {
-                const idx = queue.shift();
-                check[idx] = 1;
-                for(let j = 0; j < n; j++) {
-                    if (!check[j] && computers[j][idx]) queue.push(j);
-                    
-                }
+        visited.push(false);
+    }
+    function dfs(com, i) {
+        if (visited[i]) return;
+        visited[i] = true;
+        com[i].forEach((connection, idx) => {
+            if (connection) {
+                dfs(com, idx);
             }
-        }        
+        });
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if(!visited[i]) answer++;
+        dfs(computers, i);
     }
     return answer;
+    
+    
+    
+    
+    
+    // bfs
+//     let answer = 0;
+//     const check = computers.map(() => 0);
+//     for (let i = 0; i < n; i++) {
+//         if(!check[i]) {
+//             answer++;
+//             // bfs
+//             const queue = [i];
+//             while (queue.length > 0) {
+//                 const idx = queue.shift();
+//                 check[idx] = 1;
+//                 for(let j = 0; j < n; j++) {
+//                     if (!check[j] && computers[j][idx]) queue.push(j);
+                    
+//                 }
+//             }
+//         }        
+//     }
+//     return answer;
 }
