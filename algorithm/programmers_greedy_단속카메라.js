9f2ced77200ab9f95336a39b2897d5542f183984
JunyupHong <junyup.hong@gmail.com>
2020-09-09 00:44:40 +0900

    programmers greedy_단속카메라.js

--- algorithm/programmers_greedy_단속카메라.js
+++ algorithm/programmers_greedy_단속카메라.js
@@ -2,8 +2,7 @@ function solution(routes) {
 	let answer = 1;
     let cameraCoverage = routes[0][1];
     
-	routes.sort((a, b) => a[0] - b[0]);
-    
+	routes.sort((a, b) => a[0] - b[0]);    
 	for (let i = 0; i < routes.length - 1; i++) {
         if (cameraCoverage >= routes[i+1][0]) cameraCoverage = Math.min(cameraCoverage, routes[i+1][1]);
         else {
