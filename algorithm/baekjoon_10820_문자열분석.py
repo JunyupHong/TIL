cb3d5e141fecb4c7168c79ae42000b3bbe5284cf
JunyupHong <junyup.hong@gmail.com>
2020-10-05 01:11:30 +0900

    baekjoon_10820_문자열분석.py

--- algorithm/baekjoon_10820_문자열분석.py
+++ algorithm/baekjoon_10820_문자열분석.py
@@ -8,7 +8,7 @@ for string in stdin:
 	for char in string:
 		ascii_num = ord(char)
 		if 48 <= ascii_num <= 57: number += 1
-		elif 65 <= ascii_num <= 91: upper += 1
-		elif 97 <= ascii_num <= 123: lower += 1
+		elif 65 <= ascii_num <= 90: upper += 1
+		elif 97 <= ascii_num <= 122: lower += 1
 		elif ascii_num == 32: space += 1
 	print(lower, upper, number, space)
