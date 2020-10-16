528c4f86cc60be16fa4e73f88290bc98aec6eca5
JunyupHong <junyup.hong@gmail.com>
2020-10-16 14:01:25 +0900

    programmers_3진법뒤집기.py

--- algorithm/programmers_3진법뒤집기.py
+++ algorithm/programmers_3진법뒤집기.py
@@ -5,6 +5,19 @@ def solution(n):
     while (num > 0):
         base3 += str(num % 3)
         num = num // 3 # 몫 구하기
-    for idx, base in enumerate(base3):
-        answer += 3 ** (len(base3) - idx - 1) * int(base)
+
+    for idx in range(len(base3)): answer += (3 ** idx) * int(base3[-1 * (idx+1)])
     return answer
+
+
+# def solution(n):
+#     answer = 0
+#     num = n
+#     base3 = ""
+#     while (num > 0):
+#         base3 += str(num % 3)
+#         num = num // 3 # 몫 구하기
+#     for idx, val in enumerate(base3):
+#         answer += 3 ** (len(base3) - idx - 1) * int(val)
+#     return answer
+
