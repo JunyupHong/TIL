1cbebd3537c04afb8a094608281ee92ae003a851
JunyupHong <junyup.hong@gmail.com>
2020-10-20 12:09:42 +0900

    programmers_2019_KAKAO_크레인인형뽑기.py 다시 풀기

--- algorithm/programmers_2019_KAKAO_크레인인형뽑기.py
+++ algorithm/programmers_2019_KAKAO_크레인인형뽑기.py
@@ -23,3 +23,29 @@ def solution(board, moves):
                 stack.append(pick)
     
     return answer
+
+# 두번째
+from collections import deque
+
+def solution(board, moves):
+    answer = 0
+    answer_stack = []
+    board_stack = []
+    
+    # board를 column 기준으로 2차원 배열 만들기
+    for i in range(len(board)):
+        board_stack.append([])
+        for j in range(len(board[0]) - 1, -1, -1):
+            if board[j][i] != 0: board_stack[i].append(board[j][i])
+    
+    # moves에서 인형 뽑기
+    for n in moves:
+        if len(board_stack[n - 1]) > 0:
+            item = board_stack[n - 1].pop()
+            if len(answer_stack) > 0 and answer_stack[-1] == item:
+                answer_stack.pop();
+                answer += 2
+            else:
+                answer_stack.append(item)
+    
+    return answer
