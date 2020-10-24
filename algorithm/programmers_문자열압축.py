2edd40228811b946099ffc7b85d8f5176ce19c8c
JunyupHong <junyup.hong@gmail.com>
2020-10-24 12:36:42 +0900

    programmers_문자열압축.py

---
+++ algorithm/programmers_문자열압축.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+def solution(s):
+    answer = len(s)
+    length = answer
+
+    for n in range(1, length+1):
+        divided_str = list(filter(lambda ele: ele != '', [s[n*i : n*(i+1)] for i in range(length//n + 1)]))
+        result = [divided_str[0]]
+        count = 1
+        for i in range(1, len(divided_str)):
+            if result[-1] == divided_str[i]: count += 1
+            else:
+                result.extend([count, divided_str[i]])
+                count = 1
+        result.append(count)
+        answer = min(answer, reduce(lambda acc, cur: acc + len(cur), list(map(str, list(filter(lambda ele: ele != 1, result)))), 0))
+    return answer
