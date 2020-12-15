1993759d6100a040c670acc142f16f7bb8f744bf
JunyupHong <junyup.hong@gmail.com>
2020-12-15 14:43:14 +0900

    programmers_정수삼각형.py

--- algorithm/programmers_정수삼각형.py
+++ algorithm/programmers_정수삼각형.py
@@ -1,7 +1,18 @@
+# def solution(triangle):
+#     for height in range(1, len(triangle)):
+#         length = len(triangle[height])
+#         for i in range(length):
+#             triangle[height][i] += max(triangle[height-1][i-1 if i != 0 else 0], triangle[height-1][i if i != length-1 else -1])
+
+#     return max(triangle[-1])
+
+
 def solution(triangle):
     for height in range(1, len(triangle)):
-        length = len(triangle[height])
-        for i in range(length):
-            triangle[height][i] += max(triangle[height-1][i-1 if i != 0 else 0], triangle[height-1][i if i != length-1 else -1])
+        
+        triangle[height][0] += triangle[height-1][0]
+        for i in range(1, len(triangle[height])-1):
+            triangle[height][i] += max(triangle[height-1][i-1], triangle[height-1][i])
+        triangle[height][-1] += triangle[height-1][-1]
 
     return max(triangle[-1])
