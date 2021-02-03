0345790a4e4cbe6101e8e856cc10945ce0b9766d
JunyupHong <junyup.hong@gmail.com>
2021-02-03 17:39:23 +0900

    BOJ_15652_N과M(4).java

---
+++ algorithm/BOJ_15652_N과M(4).java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	static StringBuilder sb;
+	static int N;
+	static int M;
+
+	public static void main(String[] args) throws IOException {
+		sb = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = in.readLine();
+		N = str.charAt(0) - '0';
+		M = str.charAt(2) - '0';
+		
+		combination(0, new int[M]);
+		System.out.println(sb.toString());
+	}
+	
+	
+	static void combination(int dest, int[] numbers) {
+		if (dest == M) {
+			for (int i = 0; i < numbers.length; i++) {
+				sb.append(numbers[i]).append(" ");	
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (dest > 0 && numbers[dest - 1] > i) continue;
+			numbers[dest] = i;
+			combination(dest + 1, numbers);
+		}
+	}
+}
+
