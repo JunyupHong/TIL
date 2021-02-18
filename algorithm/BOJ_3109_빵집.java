f9f533a226f343d083a9f4e91fbcd76b595e2788
JunyupHong <junyup.hong@gmail.com>
2021-02-18 16:13:49 +0900

    BOJ_3109_빵집.java

---
+++ algorithm/BOJ_3109_빵집.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	static int R;
+	static int C;
+	static String[][] map;
+	static boolean[][] isVisited;
+	static int result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] inputs = in.readLine().split(" ");
+		R = Integer.parseInt(inputs[0]);
+		C = Integer.parseInt(inputs[1]);
+		
+		isVisited = new boolean[R][C];
+		map = new String[R][C];
+		for (int i = 0; i < R; i++) {
+			map[i] = in.readLine().split("");
+		}
+		result = 0;
+		for(int i = 0; i < R; i++) {
+			recursive(i, 0);
+		}
+		
+		System.out.println(result);
+	}
+	
+	static boolean recursive(int row, int col) {
+		if (map[row][col].equals("x")) return false;
+		if (col == C-1) {
+			result++;
+			return true;
+		}
+
+		isVisited[row][col] = true;
+		if (row > 0 && !isVisited[row-1][col+1]) {
+			if(recursive(row-1, col+1)) return true;
+		}
+		if (!isVisited[row][col+1]) if (recursive(row, col+1)) return true;
+		if (row < R-1 && !isVisited[row+1][col+1]) {
+			if (recursive(row+1, col+1)) return true;
+		}
+		
+		return false;
+	}
+}
+
