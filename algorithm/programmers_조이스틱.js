927ab9bd73f246c3ec1b39f1f10a5320b4196cea
JunyupHong <junyup.hong@gmail.com>
2021-03-06 00:15:24 +0900

    programmers_조이스틱.js

--- algorithm/programmers_조이스틱.js
+++ algorithm/programmers_조이스틱.js
@@ -2,23 +2,24 @@ function solution(name) {
     let answer = 0;
     const chars = name.split("");
     let idx = 0;
+    
     while (true) {
         const first = chars.findIndex(c => c !== "A");
         const last = chars.length - chars.reverse().findIndex(c => c !== "A") - 1;
         chars.reverse();
         if (first === -1) break;
-
-        if (Math.abs(idx - first) > Math.abs(chars.length - last + idx)) {
-            idx = last;
-            answer += Math.abs(chars.length - last + idx);
-        } else {
+        
+        if (Math.abs(first - idx) <= Math.abs(chars.length - last + idx)) {
+            answer += Math.abs(first - idx);
             idx = first;
-            answer += Math.abs(idx - first);
+        } else {
+            
+            answer += Math.abs(chars.length - last + idx);
+            idx = last;
         }
-        answer += chars[idx].charCodeAt(0) - 65;
-        console.log(answer);
+        answer += Math.min( chars[idx].charCodeAt(0) - "A".charCodeAt(0),
+                           "Z".charCodeAt(0) - chars[idx].charCodeAt(0) + 1 );
         chars[idx] = "A";
     }
-    
     return answer;
 }
