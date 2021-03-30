d2f56811a133114f5cf66871a6a7dbd62db32359
JunyupHong <junyup.hong@gmail.com>
2021-03-31 00:05:26 +0900

    BOJ_17103_골드바흐파티션.java

---
+++ algorithm/BOJ_17103_골드바흐파티션.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class BOJ_17103 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(in.readLine());
+		
+		int[] numbers = new int[T];
+		int max = Integer.MIN_VALUE;
+		HashSet<Integer> primes = new HashSet<Integer>();
+		
+		for (int i = 0; i < T; i++) {
+			numbers[i] = Integer.parseInt(in.readLine());
+			max = Math.max(max, numbers[i]);
+		}
+		for (int i = 2; i <= max; i++) {
+			primes.add(i);
+		}
+		for (int i = 2; i <= max/2; i++) {
+			for (int j = 2; i*j <= max; j++) {
+				primes.remove(i*j);
+			}
+		}
+		int[] primeArr = new int[primes.size()];
+		int cnt = 0;
+		for (int p : primes) {
+			primeArr[cnt++] = p;
+		}
+
+		for (int n = 0; n < numbers.length; n++) {
+			int answer = 0;
+			for (int a : primes) {
+				if (a > numbers[n]) break;
+				if (primes.contains(numbers[n] - a)) {
+					if (numbers[n] - a >= a) answer++;
+				}
+			}
+			sb.append(answer).append("\n");
+		}
+		System.out.print(sb.toString());
+	}
+}
+
