830f0929200ffedf11cd1ab6ebc1eb99d5dbf342
JunyupHong <junyup.hong@gmail.com>
2021-04-01 13:19:17 +0900

    BOJ_17103_골드바흐파티션.java

--- algorithm/BOJ_17103_골드바흐파티션.java
+++ algorithm/BOJ_17103_골드바흐파티션.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 
-public class BOJ_17103 {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
