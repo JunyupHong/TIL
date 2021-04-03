bf592246ea6b062b117db7076fb4259de8b198f3
JunyupHong <junyup.hong@gmail.com>
2021-04-03 13:40:43 +0900

    BOJ_1242_소풍.java

--- algorithm/BOJ_1242_소풍.java
+++ algorithm/BOJ_1242_소풍.java
@@ -8,16 +8,17 @@ public class Main {
 		String[] inputs = in.readLine().split(" ");
 		int N = Integer.parseInt(inputs[0]);
 		int K = Integer.parseInt(inputs[1]);
-		int M = Integer.parseInt(inputs[2]) - 1;		
+		int M = Integer.parseInt(inputs[2]) - 1; // idx로 계산할꺼니까 -1 처리
+		
 		int answer = 0;
 		int removeIdx = 0;
 		while (true) {
 			removeIdx += K-1;
-			removeIdx %= N;
-			answer++;
-			if (removeIdx == M) break;
-			if (removeIdx < M) M--;
-			N--;
+			removeIdx %= N; // 지워야할 사람을 찾음
+			answer++; // 지움
+			if (removeIdx == M) break; // 지운게 M이면 종료
+			if (removeIdx < M) M--; // M보다 앞 사람이 지워지면 M을 앞으로 떙김
+			N--; // 전체 인원이 1명 줄어듬
 		}
 		
 		System.out.println(answer);
