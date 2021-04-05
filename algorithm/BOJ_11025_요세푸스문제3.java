4faae05ceb9e6d3637aea080c4780fdbafcc2645
JunyupHong <junyup.hong@gmail.com>
2021-04-05 18:57:06 +0900

    BOJ_11025_요세푸스문제3.java

--- algorithm/BOJ_11025_요세푸스문제3.java
+++ algorithm/BOJ_11025_요세푸스문제3.java
@@ -8,13 +8,12 @@ public class Main {
 		
 		int N = Integer.parseInt(inputs[0]);
 		int K = Integer.parseInt(inputs[1]);
-		int[] dp = new int[N+1];
-		dp[1] = 1;
+		int dp = 1;
 		for (int i = 2; i <= N; i++) {
-			dp[i] = (dp[i-1] + K-1) % i + 1;
+			dp = (dp + K-1) % i + 1;
 		}
 		
-		System.out.println(dp[N]);
+		System.out.println(dp);
 	}
 }
 
