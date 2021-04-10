23e8bd27e14b1a0cf01d6e992c77380ab30fddf6
JunyupHong <junyup.hong@gmail.com>
2021-04-10 17:02:11 +0900

    SWEA_5644_무선충전.java

--- algorithm/SWEA_5644_무선충전.java
+++ algorithm/SWEA_5644_무선충전.java
@@ -1,122 +1,114 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
 	static class BC implements Comparable<BC> {
-		int[] pos = new int[2];
-		int range;
-		int power;
-		public BC(int x, int y, int range, int power) {
-			this.pos[0] = x;
-			this.pos[1] = y;
-			this.range = range;
-			this.power = power;
+		public int x;
+		public int y;
+		public int distance;
+		public int performance;
+		public BC(int x, int y, int distance, int performance) {
+			this.x = x;
+			this.y = y;
+			this.distance = distance;
+			this.performance = performance;
 		}
 		
-		boolean isChargable(int[] pos) {
-			int d = Math.abs(this.pos[0] - pos[0]) + Math.abs(this.pos[1] - pos[1]);
-			if (d > this.range) return false;
-			else return true;
+		public boolean possible(int x, int y) {
+			return this.distance >= Math.abs(x - this.x) + Math.abs(y - this.y);
 		}
-
+		
 		@Override
 		public int compareTo(BC o) {
-			if (this.power > o.power) return -1;
-			else return 0;
+			return Integer.compare(o.performance, this.performance);
 		}
 	}
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String[] inputs;
+		
 		int T = Integer.parseInt(in.readLine());
-
+		int[] p1;
+		int[] p2;
+		
 		for (int tc = 1; tc <= T; tc++) {
-			inputs = in.readLine().split(" ");
+			p1 = new int[] { 1, 1 };
+			p2 = new int[] { 10, 10 };
+			String[] inputs = in.readLine().split(" ");
 			int M = Integer.parseInt(inputs[0]);
 			int A = Integer.parseInt(inputs[1]);
-			String[] aMove = in.readLine().split(" ");
-			String[] bMove = in.readLine().split(" ");
-			int[] aPos = new int[] {1, 1};
-			int[] bPos = new int[] {10, 10};
 			
-			int chargeAmount = 0;
-			ArrayList<BC> bc = new ArrayList<BC>();
+			String[] p1Moves = in.readLine().split(" ");
+			String[] p2Moves = in.readLine().split(" ");
+			
+			BC[] BCs = new BC[A];
 			for (int i = 0; i < A; i++) {
 				inputs = in.readLine().split(" ");
-				bc.add(new BC(Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]), Integer.parseInt(inputs[3])));
+				BCs[i] = new BC(Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]), Integer.parseInt(inputs[3]));
 			}
 			
-			bc.sort(null);
+			Arrays.sort(BCs);
 			
-			for (BC cur : bc) {
-				if (cur.isChargable(aPos)) {
-					chargeAmount += cur.power;
-					break;
+			int result = 0;
+			
+			for (int m = 0; m <= M; m++) {
+				ArrayList<BC> p1Chargable = new ArrayList<BC>();
+				ArrayList<BC> p2Chargable = new ArrayList<BC>();
+				for (BC bc : BCs) {
+					if (bc.possible(p1[0], p1[1])) p1Chargable.add(bc);
+					if (bc.possible(p2[0], p2[1])) p2Chargable.add(bc);
 				}
-			}
-			for (BC cur : bc) {
-				if (cur.isChargable(bPos)) {
-					chargeAmount += cur.power;
-					break;
-				}
-			}
-
-			ArrayList<BC> aPossibleBC = new ArrayList<BC>();
-			ArrayList<BC> bPossibleBC = new ArrayList<BC>();
-			for (int t = 0; t < M; t++) {
-				aPossibleBC.clear();
-				bPossibleBC.clear();
-
-				// move
-				if (aMove[t].equals("1")) aPos[1] -= 1;
-				else if (aMove[t].equals("2")) aPos[0] += 1;
-				else if (aMove[t].equals("3")) aPos[1] += 1;
-				else if (aMove[t].equals("4")) aPos[0] -= 1;
-				if (bMove[t].equals("1")) bPos[1] -= 1;
-				else if (bMove[t].equals("2")) bPos[0] += 1;
-				else if (bMove[t].equals("3")) bPos[1] += 1;
-				else if (bMove[t].equals("4")) bPos[0] -= 1;
-				
-				for (BC cur : bc) {
-					if (cur.isChargable(aPos)) aPossibleBC.add(cur);
-					if (cur.isChargable(bPos)) bPossibleBC.add(cur);
-				}
-				
-				if (aPossibleBC.size() == 0 && bPossibleBC.size() == 0) {
-					// nothing
-				} else if (aPossibleBC.size() == 0 || bPossibleBC.size() == 0) {
-					if (aPossibleBC.size() > 0) chargeAmount += aPossibleBC.get(0).power;
-					else chargeAmount += bPossibleBC.get(0).power;
-				} else {
-					BC aTemp = aPossibleBC.get(0);
-					BC bTemp = bPossibleBC.get(0);
-					if (aTemp == bTemp) {
-						if (aPossibleBC.size() > 1 && bPossibleBC.size() > 1) {
-							if (aPossibleBC.get(1).power > bPossibleBC.get(1).power) {
-								aTemp = aPossibleBC.get(1);
-							} else {
-								bTemp = bPossibleBC.get(1);
-							}
-						} else if (aPossibleBC.size() > 1) {
-							aTemp = aPossibleBC.get(1);
-						} else if (bPossibleBC.size() > 1) {
-							bTemp = bPossibleBC.get(1);
-						}
-					}
-					if (aTemp == bTemp) {
-						chargeAmount += aTemp.power;
+				if (p1Chargable.size() == 0 || p2Chargable.size() == 0) {
+					if (p1Chargable.size() > 0) result += p1Chargable.get(0).performance;
+					if (p2Chargable.size() > 0) result += p2Chargable.get(0).performance;
+				} else if (p1Chargable.size() == 1 && p2Chargable.size() == 1) {
+					if (p1Chargable.get(0) == p2Chargable.get(0)) {
+						result += p1Chargable.get(0).performance;
 					} else {
-						chargeAmount += aTemp.power;
-						chargeAmount += bTemp.power;
+						result += p1Chargable.get(0).performance;
+						result += p2Chargable.get(0).performance;
+					}
+				} else if (p1Chargable.size() == 1 ||  p2Chargable.size() == 1) {
+					if (p1Chargable.get(0) == p2Chargable.get(0)) {
+						if (p2Chargable.size() > 1) {
+							result += p1Chargable.get(0).performance;
+							result += p2Chargable.get(1).performance;
+						} else {
+							result += p1Chargable.get(1).performance;
+							result += p2Chargable.get(0).performance;
+						}
+					} else {
+						result += p1Chargable.get(0).performance;
+						result += p2Chargable.get(0).performance;
+					}
+				} else if (p1Chargable.size() > 1 && p2Chargable.size() > 1) {
+					if (p1Chargable.get(0) == p2Chargable.get(0)) {
+						result += p1Chargable.get(0).performance;
+						result += Math.max(p1Chargable.get(1).performance, p2Chargable.get(1).performance);
+					} else {
+						result += p1Chargable.get(0).performance;
+						result += p2Chargable.get(0).performance;
 					}
 				}
+				
+				if (m == M) break;
+
+				if (p1Moves[m].equals("1")) p1[1] -= 1;
+				else if (p1Moves[m].equals("2")) p1[0] += 1;
+				else if (p1Moves[m].equals("3")) p1[1] += 1;
+				else if (p1Moves[m].equals("4")) p1[0] -= 1;
+				
+				if (p2Moves[m].equals("1")) p2[1] -= 1;
+				else if (p2Moves[m].equals("2")) p2[0] += 1;
+				else if (p2Moves[m].equals("3")) p2[1] += 1;
+				else if (p2Moves[m].equals("4")) p2[0] -= 1;				
 			}
-			
-			sb.append("#").append(tc).append(" ").append(chargeAmount).append("\n");
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
+		
 		System.out.println(sb.toString());
 	}
 }
+
