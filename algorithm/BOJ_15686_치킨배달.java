120c005281574c4859144f67cb198092eeedf8b7
JunyupHong <junyup.hong@gmail.com>
2021-04-20 00:03:42 +0900

    BOJ_15686_치킨배달.java

--- algorithm/BOJ_15686_치킨배달.java
+++ algorithm/BOJ_15686_치킨배달.java
@@ -1,54 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-class Main {
-	static ArrayList<int[]> chicken;
-	static ArrayList<int[]> house;
-	static int N;
-	static int M;
+public class Main {
 	static int result;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String[] inputs = in.readLine().split(" ");
-		N = Integer.parseInt(inputs[0]);
-		M = Integer.parseInt(inputs[1]);
+
 		result = Integer.MAX_VALUE;
-		int[][] map = new int[N][N];
+		String[] inputs = in.readLine().split(" ");
 		
-		house = new ArrayList<int[]>();
-		chicken = new ArrayList<int[]>();
+		int N = Integer.parseInt(inputs[0]);
+		int M = Integer.parseInt(inputs[1]);
+		
+		int[][] map = new int[N][N];
+		ArrayList<int[]> house = new ArrayList<int[]>();
+		ArrayList<int[]> chicken = new ArrayList<int[]>();
+		
 		for (int i = 0; i < N; i++) {
 			inputs = in.readLine().split(" ");
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(inputs[j]);
-				if (inputs[j].equals("1")) house.add(new int[] {i, j});
-				else if (inputs[j].equals("2")) chicken.add(new int[] {i, j});
+				if (map[i][j] == 1) house.add(new int[] { i, j });
+				else if (map[i][j] == 2) chicken.add(new int[] { i, j });
 			}
 		}
-		
-		combination(0, new int[M][2], 0);
+		combination(house, chicken, M, 0, 0, new int[M][2]);
 		System.out.println(result);
+		
 	}
 	
-	static void combination(int dest, int[][] selected, int start) {
-		if (dest == M) {
-			int totalDistance = 0;
-			for (int[] h : house) {
-				int chickenDistance = Integer.MAX_VALUE;
-				for (int[] s : selected) {
-					chickenDistance = Math.min(chickenDistance, Math.abs(h[0] - s[0]) + Math.abs(h[1] - s[1]));
+	static void combination(ArrayList<int[]> house, ArrayList<int[]> chicken, int M, int dest, int start, int[][] selected) {
+		if (M == dest) {
+			int total = 0;
+			for (int i = 0; i < house.size(); i++) {
+				int min = Integer.MAX_VALUE;
+				for (int j = 0; j < M; j++) {
+					min = Math.min(min, getDistance(house.get(i), selected[j]));
 				}
-				totalDistance += chickenDistance;
+				total += min;
 			}
-			result = Math.min(result, totalDistance);
+			result = Math.min(result, total);
 			return;
 		}
 		for (int i = start; i < chicken.size(); i++) {
 			selected[dest] = chicken.get(i);
-			combination(dest+1, selected, i+1);
+			combination(house, chicken, M, dest+1, i+1, selected);
 		}
+		
+	}
+	
+	static int getDistance(int[] start, int[] dest) {
+		return Math.abs(start[0] - dest[0]) + Math.abs(start[1] - dest[1]);
 	}
 }
 
