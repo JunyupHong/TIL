245a3e001a2b720745f6f5f44f65e56f9c311186
JunyupHong <junyup.hong@gmail.com>
2021-04-23 00:05:15 +0900

    SWEA_5643_키순서.java

---
+++ algorithm/SWEA_5643_키순서.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+	static int N, M;
+	static int adj[][];
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(in.readLine());
+		String[] inputs;
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(in.readLine());
+			M = Integer.parseInt(in.readLine());
+			
+			adj = new int[N+1][N+1];
+			for (int m = 0; m < M; m++) {
+				inputs = in.readLine().split(" ");
+				int i = Integer.parseInt(inputs[0]);
+				int j = Integer.parseInt(inputs[1]);
+				
+				adj[i][j] = 1;
+			}
+			
+			int answer = 0;
+			for (int k = 1; k <= N; k++) {
+				if (gtBFS(k, new boolean[N+1]) + ltBFS(k, new boolean[N+1]) == N-1) answer++;
+			}
+			
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	private static int gtBFS(int start, boolean[] visited) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(start);
+		visited[start] = true;
+		
+		int cnt = 0;
+		while (!queue.isEmpty()) {
+			int k = queue.poll();
+			for (int i = 1; i <= N; i++) {
+				if (visited[i] || adj[k][i] != 1) continue;
+				
+				queue.offer(i);
+				visited[i] = true;
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	private static int ltBFS(int start, boolean[] visited) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(start);
+		visited[start] = true;
+		
+		int cnt = 0;
+		while (!queue.isEmpty()) {
+			int k = queue.poll();
+			for (int i = 1; i <= N; i++) {
+				if (visited[i] || adj[i][k] != 1) continue;
+				
+				queue.offer(i);
+				visited[i] = true;
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+}
+
